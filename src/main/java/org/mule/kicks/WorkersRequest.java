edca75aa8624034e12909865c37cd75b1686f5ad
Miguel Oliva <moliva@mulesoft.com>
2014-03-06 15:39:36 -0300

    Refactored WorkersRequest. Delete extra flows and processors. Added poll with watermark for workday

--- src/main/java/org/mule/kicks/WorkersRequest.java
+++ src/main/java/org/mule/kicks/WorkersRequest.java
@@ -1,8 +1,6 @@
 package org.mule.kicks;
 
-import java.math.BigDecimal;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -12,62 +10,41 @@ import javax.xml.datatype.XMLGregorianCalendar;
 
 import com.workday.hr.EffectiveAndUpdatedDateTimeDataType;
 import com.workday.hr.GetWorkersRequestType;
-import com.workday.hr.ResponseFilterType;
 import com.workday.hr.TransactionLogCriteriaType;
-import com.workday.hr.TransactionLogTypeObjectType;
-import com.workday.hr.TransactionTypeReferencesType;
 import com.workday.hr.WorkerRequestCriteriaType;
 
 public class WorkersRequest {
 
-	public static GetWorkersRequestType create() throws ParseException, DatatypeConfigurationException {
+	public static GetWorkersRequestType create(Date startDate) throws ParseException, DatatypeConfigurationException {
 
-		Date startDate = new SimpleDateFormat("yyyy-MM-dd").parse("2014-02-26");
-//		Date startDate = new SimpleDateFormat("yyyy-MM-dd").parse("2014-01-06");
-		GregorianCalendar gregorianCalendarStart = (GregorianCalendar) GregorianCalendar.getInstance();
-		gregorianCalendarStart.setTime(startDate);
-		XMLGregorianCalendar xmlCalendarStart = DatatypeFactory.newInstance()
-																.newXMLGregorianCalendar(gregorianCalendarStart);
-
-//		Date endDate = new Date();
-		Date endDate = new SimpleDateFormat("yyyy-MM-dd").parse("2014-02-26");
-		GregorianCalendar gregorianCalendarEnd = (GregorianCalendar) GregorianCalendar.getInstance();
-		gregorianCalendarEnd.setTime(endDate);
-		XMLGregorianCalendar xmlCalendarEnd = DatatypeFactory.newInstance()
-																.newXMLGregorianCalendar(gregorianCalendarEnd);
+		/*
+		 * Set data range for events
+		 */
 
 		EffectiveAndUpdatedDateTimeDataType dateRangeData = new EffectiveAndUpdatedDateTimeDataType();
-		dateRangeData.setEffectiveFrom(xmlCalendarStart);
-		dateRangeData.setEffectiveThrough(xmlCalendarEnd);
+		dateRangeData.setUpdatedFrom(xmlDate(startDate));
+		dateRangeData.setUpdatedThrough(xmlDate(new Date()));
 
-//		TransactionLogTypeObjectIDType transactionTypeId = new TransactionLogTypeObjectIDType();
-//		transactionTypeId.setType("Business_Process_Type");
-//		transactionTypeId.setValue("Hire Employee");
-
-		TransactionLogTypeObjectType transactionTypeReference = new TransactionLogTypeObjectType();
-//		transactionTypeReference.getID()
-//								.add(transactionTypeId);
-
-		TransactionTypeReferencesType transactionTypeReferences = new TransactionTypeReferencesType();
-		transactionTypeReferences.getTransactionTypeReference()
-									.add(transactionTypeReference);
+		/*
+		 * Set event type criteria filter
+		 */
 
 		TransactionLogCriteriaType transactionLogCriteria = new TransactionLogCriteriaType();
-		transactionLogCriteria.setTransactionTypeReferences(transactionTypeReferences);
 		transactionLogCriteria.setTransactionDateRangeData(dateRangeData);
 
 		WorkerRequestCriteriaType workerRequestCriteria = new WorkerRequestCriteriaType();
-		workerRequestCriteria.getTransactionLogCriteriaData()
-								.add(transactionLogCriteria);
-
-		ResponseFilterType responseFilterType = new ResponseFilterType();
-		responseFilterType.setPage(new BigDecimal(1));
+		workerRequestCriteria.getTransactionLogCriteriaData().add(transactionLogCriteria);
 
 		GetWorkersRequestType getWorkersType = new GetWorkersRequestType();
-		getWorkersType.setResponseFilter(responseFilterType);
 		getWorkersType.setRequestCriteria(workerRequestCriteria);
 
 		return getWorkersType;
+	}
+
+	private static XMLGregorianCalendar xmlDate(Date date) throws DatatypeConfigurationException {
+		GregorianCalendar gregorianCalendar = (GregorianCalendar) GregorianCalendar.getInstance();
+		gregorianCalendar.setTime(date);
+		return DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
 	}
 
 }
