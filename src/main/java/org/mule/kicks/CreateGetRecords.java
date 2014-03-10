64e24c1fb84a057ee7b8bebea4b094f9bf526350
Javier Casal <javier.casal@mulesoft.com>
2014-03-10 09:38:53 -0300

    updated mapping for update, commented batch processing until Jira EE-3866 (https://www.mulesoft.org/jira/browse/EE-3866) is solved, updated lastQueryDate expression, parametrized CreateGetRecords transformer class

--- src/main/java/org/mule/kicks/CreateGetRecords.java
+++ src/main/java/org/mule/kicks/CreateGetRecords.java
@@ -1,29 +1,31 @@
 package org.mule.kicks;
 
+import java.util.List;
+
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
 
 import com.servicenow.sysuser.GetRecords;
+import com.workday.hr.WorkerDataType;
+import com.workday.hr.WorkerType;
 
 public class CreateGetRecords extends AbstractMessageTransformer {
 
 	@Override
 	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
 
-//		WorkerType payload = (WorkerType) message.getPayload();
-//		WorkerDataType workerData = payload.getWorkerData();
+		@SuppressWarnings("unchecked")
+		List<WorkerType> payload = (List<WorkerType>) message.getPayload();
+		WorkerType workerType = payload.get(0);
+		WorkerDataType workerData = workerType.getWorkerData();
 		
 		GetRecords query = new GetRecords();
 
-//		query.setFirstName(workerData.getPersonalData().getNameData().getLegalNameData().getNameDetailData().getFirstName());
-//		query.setLastName(workerData.getPersonalData().getNameData().getLegalNameData().getNameDetailData().getLastName());
-//		query.setEmail(workerData.getPersonalData().getContactData().getEmailAddressData().get(0).getEmailAddress());
+		query.setFirstName(workerData.getPersonalData().getNameData().getLegalNameData().getNameDetailData().getFirstName());
+		query.setLastName(workerData.getPersonalData().getNameData().getLegalNameData().getNameDetailData().getLastName());
+		query.setEmail(workerData.getPersonalData().getContactData().getEmailAddressData().get(0).getEmailAddress());
 		
-		query.setFirstName("Javier");
-		query.setLastName("Casal");
-		query.setEmail("javier@mail.com");
-
 		message.setPayload(query);
 		return message;
 	}
